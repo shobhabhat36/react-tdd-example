83637bfc4ddd1cf30a358c910da86657be96ea97
Shobha Bhat <shobha.bhat36@gmail.com>
2021-05-20 23:11:00 +0530

    Added counter initial state and rendered in a div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <h1>Welcome to counter app</h1>
+      <div id="counter-value">{counter}</div>
       <button id="increment-btn">Increment</button>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -6,14 +6,20 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 configure({ adapter: new Adapter() });
 
 describe('Counter Test', () => {
+  let wrapper;
+  beforeEach(()=>{
+    wrapper = shallow(<App />);
+  });
   test('should render counter app welcome message', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('h1').text()).toContain('Welcome to counter app');
   });
 
   test('should render a button with text `Increment`', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('#increment-btn').text()).toBe('Increment');
+  });
+  
+  test('should render the initial value of state in a div', () => {
+    expect(wrapper.find('#counter-value').text()).toBe("0");
   });
   
 });
