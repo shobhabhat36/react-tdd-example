26ea2f69870ac58045214c1dbabac10f35830bdf
Shobha Bhat <shobha.bhat36@gmail.com>
2021-05-20 21:51:20 +0530

    Initial test - Counter app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Counter Test', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText('Welcome to counter app');
+    expect(linkElement).toBeInTheDocument();
+  });
 });
